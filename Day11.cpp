301631b0b7d9cf6f835f99528f120d1603b2a4d2
BoyKa74 <maivusongkuku09@gmail.com>
2025-03-14 09:53:35 +0700

    Day11

---
+++ Day11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    // create integer variable
+    int num1 = 54;
+
+    // create double variable
+    double num2 = 88.99;
+
+    // print variables
+    std::cout << num1 << " " << num2;
+
+    return 0;
+}
