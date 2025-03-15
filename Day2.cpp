6a338c3be58e63b1c3e17b33f766a53c5386dcc4
BoyKa74 <maivusongkuku09@gmail.com>
2025-03-15 21:29:46 +0700

    Add C++ files

---
+++ Day2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+
+	std::cout << "Learn C++ with TEK4.VN!";
+}
---
+++ Day4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "C++ is hard" << endl;
+	cout << "C++ is easy" << endl;
+}
